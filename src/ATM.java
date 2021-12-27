757905ca3b84efdffa9ea2e2c8ed3fd3ce8c049a
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2021-12-27 21:28:11 +0330

    Added Balance Check

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,49 @@
+import java.util.Scanner;
 
 public class ATM {
 
+	String name;
+	int cardNumber;
+	private int balance = 0;
+	
+	ATM(String name, int cardNumber) {
+		this.name = name;
+		this.cardNumber = cardNumber;
+	}
+	
+	public void checkBalance() {
+		System.out.println("Your Balance is " + this.balance);
+	}
+	
+	public void showMenu() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hello " + this.name + " With Card Number " + this.cardNumber);
+		System.out.println("Check Balance ==> B");
+		System.out.println("Deposit ==> D");
+		System.out.println("Withdraw ==> W");
+		System.out.println("Previous Transaction ==> T");
+		System.out.println("Exit ==> E\n");
+		
+		System.out.println("**************************************");
+		System.out.println("Please Choose Your Option: ");
+		System.out.println("**************************************");
+		char c = scanner.next().charAt(0);
+		c = Character.toUpperCase(c);
+		System.out.println("");
+		switch (c) {
+			case 'B':
+				System.out.println("----------------------------------");
+				this.checkBalance();
+				System.out.println("----------------------------------\n");
+				break;
+			default:
+				System.out.println("Invalid Option");
+		}
+		
+		System.out.println("GoodBye :)");
+		scanner.close();
+	}
+	
+	
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("***** Welcome To The Best ATM in The World *****\n");
+		// Ask User's Name
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("**************************************");
+		System.out.println("Please Enter Your Name: ");
+		System.out.println("**************************************");
+		String name = scanner.next();
+		System.out.println("");
+		
+		// Ask User's Card Number		
+		System.out.println("**************************************");
+		System.out.println("Please Enter You'r Card Number: ");
+		System.out.println("**************************************");
+		int cardNumber = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("");
+		
+		ATM person = new ATM(name, cardNumber);
+		person.showMenu();
+		
+		scanner.close();
 	}
 
 }
